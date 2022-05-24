adcc1ffbafddd8ecd71938e1c91f45e643fdd928
Lim Yao cheng <lyao-che@u81z04s03.42kl.edu.my>
2022-05-24 08:28:17 +0800

    Updated ft_atoi.c

---
+++ 00_Libft/ft_atoi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 08:22:58 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/05/24 08:27:35 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	num;
+	int	sign;
+
+	num = 0;
+	sign = 1;
+
+	while (*nptr == ' ' || (*nptr >= '\t' && *nptr <= '\r'))
+		nptr++;
+	if (*nptr == '-')
+		sign = -1;
+	while (*nptr != '\0')
+	{
+		if (*nptr >= '0' && *nptr <= '9')
+			num = (*nptr - '0') + (num * 10);
+		nptr++;
+	}
+	return (num * sign);
+}
