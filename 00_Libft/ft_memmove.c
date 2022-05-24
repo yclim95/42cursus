0fa8523b6027929e7bde90099b0609d8a09dbbd4
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-05-24 15:58:43 +0800

    Updated ft_memmove.c

---
+++ 00_Libft/ft_memmove.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 15:53:10 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/05/24 15:57:49 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char			*pdest;
+	unsigned char	*psrc;
+
+	if (!pdest && !psrc)
+		return (0);
+	if (pdest > psrc)
+	{
+		pdest = dest + n;
+		psrc = src + n;
+		while (n-- > 0)
+		{
+			pdest--;
+			psrc--;
+			pdest = psrc;
+		}
+	}
+	else
+	{
+		pdest = dest;
+		psrc = src;
+		while (n-- > 0)
+		{
+			pdest = psrc;
+			pdest++;
+			psrc++;
+		}
+	}
+}
