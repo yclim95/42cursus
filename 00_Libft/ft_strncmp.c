c751f054cea77788f80920549c9236e742f678d5
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-05-25 08:30:53 +0800

    Updated ft_strncmp.c

---
+++ 00_Libft/ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 08:19:13 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/05/25 08:29:39 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	us1;
+	unsigned char	us2;
+
+	while (n-- > 0)
+	{
+		us1 = *(unsigned char*)s1++;
+		us2 = *(unsigned char*)s2++;
+		if (us1 != us2)
+			return (us1 - us2);
+		if (us1 == '\0')
+			return (0);
+	}
+	return (0);
+}
