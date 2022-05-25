4a2502961d593739d7f48e0160d7ffa7c5e5cd83
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-05-25 11:38:34 +0800

    Updated ft_strlcpy.c

---
+++ 00_Libft/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 11:29:52 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/05/25 11:33:13 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, char *src, size_t size)
+{
+	size_t	len;
+
+	len = ft_strlen(src);
+	if (len + 1 < size)
+	{
+		ft_memcpy(dest,src,len);
+		dest[len] = '\0';
+	}
+	if (size != 0)
+	{
+		ft_memcpy(dest,src,size - 1);
+		dest[size - 1] = '\0';
+	}
+	return (len);
+}
