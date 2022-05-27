2d94654bb50b86aabaa0ab0e61ddd652e5b85b76
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-05-27 11:31:31 +0800

    Updated ft_calloc.c

---
+++ 00_Libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 11:23:47 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/05/27 11:28:26 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void *result;
+
+	result = malloc(nmemb * size);
+	if (!result)
+		return (0);
+	ft_bezero(result, nmemb * size);
+	return (result);
+}
