4d9be224dd5935f44754ee50ffcef686cc97b01b
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-05-27 08:25:40 +0800

    Updated ft_strlcat.c

--- 00_Libft/ft_strlcat.c
+++ 00_Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 15:21:47 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/05/25 15:24:01 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/05/27 08:24:38 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	total_len;
 
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dest_len > size)
+		dest_len = size;
+	if (dest_len == size)
+		return (size + src_len);
+	total_len = dest_len + src_len;
+	if (src_len >= size - dest_len)
+	{
+		ft_memcpy(dest + dest_len, src, size - dest_len - 1);
+		dest[size - 1] = '\0';
+	}
+	else
+	{
+		ft_memcpy(dest + dest_len, src, src_len);
+		dest[total_len] = '\0';
+	}
+	return (total_len);
 }
