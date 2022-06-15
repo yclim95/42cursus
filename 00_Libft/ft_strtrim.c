094e4884d743b2bbee8b126f0a0ae5705e73b871
Lim Yao cheng <lyao-che@u80z03s02.42kl.edu.my>
2022-06-15 10:25:43 +0800

    Updated: ft_strtrim.c

--- 00_Libft/ft_strtrim.c
+++ 00_Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:35:46 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/06/10 14:36:43 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/06/15 10:20:12 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || !set)
 		return (NULL);
-	while (*s1 && ft_strchr(set, *s1))
+	while (*s1 && ft_strchr(set, *s1) != 0)
 		s1++;
 	len = ft_strlen(s1);
-	while (len && ft_strchr(set, s1[len]))
+	while (len && ft_strchr(set, s1[len - 1]) != 0)
 		len--;
 	ptr_trim = malloc((len + 1) * sizeof(char));
 	if (!ptr_trim)
-		return (0);
+		return (NULL);
 	ft_memcpy(ptr_trim, s1, len);
 	ptr_trim[len] = '\0';
 	return (ptr_trim);
