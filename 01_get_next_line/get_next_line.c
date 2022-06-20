fe712d42cd92bbf7d430bb93766cd260875fe33c
Lim Yao Cheng <YCLim@Lims-iMac.local>
2022-06-21 07:37:02 +0800

    Updated: get_next_line.c

--- 01_get_next_line/get_next_line.c
+++ 01_get_next_line/get_next_line.c
@@ -6,9 +6,20 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 19:27:39 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/06/19 19:43:33 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/06/21 07:36:39 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+char	*ft_read_and_save(int fd, char *save)
+{
+}
+
+char	*ft_save(char *save)
+{
+}
+
+char	*ft_getline(char *save)
+{
+}
 
 char	*get_next_line(int fd)
 {
