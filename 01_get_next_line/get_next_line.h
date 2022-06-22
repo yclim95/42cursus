2da69edd3a4354f54585a732c63d314fc4eb30da
Lim Yao Cheng <YCLim@Lims-iMac.local>
2022-06-22 11:33:13 +0800

    Updated get_next_line.h

--- 01_get_next_line/get_next_line.h
+++ 01_get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 19:10:40 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/06/19 19:26:58 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/06/22 11:32:43 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
-char	*ft_get_line(char *save);
+char	*ft_getline(char *save);
 char	*ft_save(char *save);
-char	*ft_read_and_save(int fd, char *save);
+char	*ft_read_save(int fd, char *save);
 size_t	ft_strlen(char *s);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
