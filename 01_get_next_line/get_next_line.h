b3a0f5ce748f16592f9ed55a26e9a2d82f0b0b85
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-07-18 09:28:36 +0800

    Updated header file

--- 01_get_next_line/get_next_line.h
+++ 01_get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 19:10:40 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/06/22 11:32:43 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/07/18 09:27:17 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,6 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
 
 char	*get_next_line(int fd);
 char	*ft_getline(char *save);
