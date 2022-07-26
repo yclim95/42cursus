5c9471cb6637f69d35589040e160b7e8a79b6816
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-07-26 10:50:05 +0800

    Fixed: %u %x testing

--- 02_ft_printf/sources/ft_formats.c
+++ 02_ft_printf/sources/ft_formats.c
@@ -6,7 +6,7 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 07:54:03 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/07/26 10:33:25 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/07/26 10:44:07 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int	ft_formats(int i, va_list args)
 	else if (i == 'i' || i == 'd')
 		c = ft_format_decimal(va_arg(args, int));
 	else if (i == 'u')
+		c = ft_format_unsigned_decimal(va_arg(args, unsigned long));
+	else if (i == 'x')
 		c = ft_format_hexadecimal(va_arg(args, unsigned long));
 	else if (i == 'X')
 		c = ft_format_uppercase_hexadecimal(va_arg(args, unsigned long));
