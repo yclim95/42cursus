995379a664bba193ac09556c135520a30e51af15
Lim Yao Cheng <YCLim@Lims-iMac.local>
2022-08-15 17:49:34 +0800

    Updated server.c

--- 04_minitalk/sources/server.c
+++ 04_minitalk/sources/server.c
@@ -6,7 +6,7 @@
 /*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 09:56:52 by lyao-che          #+#    #+#             */
-/*   Updated: 2022/08/15 14:32:30 by lyao-che         ###   ########.fr       */
+/*   Updated: 2022/08/15 17:49:21 by lyao-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,56 @@
 
 char	*ft_print_msg(char *msg)
 {
+	ft_putstr(msg);
+	ft_putchar('\n');
+	free(msg);
+	return (NULL);
 }
 
 void	ft_show_error_msg_exit(int pid, char *str)
 {
+	if(str)
+		free(str);
+	ft_putstr("An unexcepected error occured during server execution\n");
+	kill(pid, SIGUSR2);
+	exit(EXIT_FAILURE);
 }
 
 void	ft_handle_server_client_signal(int sig, siginfo_t *info, void *context)
+{
+	static int	bit_c;
+	static int	pid;
+	static char	sig_c;
+	static char	*msg;
+
+	bit_c = 0;
+	pid = 0;
+	sig_c = 0b11111111;
+	msg = NULL;
+	(void)context;
+	if (info->si_pid)
+		pid = info->si_pid;
+	if (sig == SIGUSR1)
+		sig_c ^= 0b10000000 >> bit_c;
+	else if (sig == SIGUSR2)
+		sig_c |= 0b10000000 >> bit_c;
+	if (++bit_c == 8)
+	{
+		if (sig_c != '\0')
+			msg = ft_add_char_to_str(msg, c);
+		else
+			msg = ft_print_msg(msg);
+		bit_c = 0;
+		sig_c = 0b11111111;
+	}
+	if (kill(pid, SIGSR1) == -1)
+		ft_show_error_msg_exit(pid, msg);
+}
 
 int main(void)
 {
 	struct	sigaction	sig;
-	sigset_t			sig_set;
+	sigset_t		sig_set;
 
 	sigemptyset(&sig_set);
 	sigaddset(&sig_set, SIGINT);
