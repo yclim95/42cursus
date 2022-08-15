54a8f125228d0dc946dfca95cb23667d462713ab
Lim Yao cheng <lyao-che@u90z04s01.42kl.edu.my>
2022-08-15 10:11:53 +0800

    Added client.c

---
+++ 04_minitalk/sources/client.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 10:07:36 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/08/15 10:11:42 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_client_error_msg_exit(char *str)
+{
+}
+
+int		ft_send_null_terminate(int pid, char *str)
+{
+}
+
+void	ft_send_msg_bit_by_bit(int pid, char *str)
+{
+}
+
+void	ft_handle_client_signal(int signum)
+{
+}
+
+int		sigemptyset(sigset_t *set)
+{
+}
+
+int		sigaction(const struct sigaction *restrict act, struct sigaction *restrict p_act, int sig)
+{
+}
+
+int		sigaddset(sigset_t *set, int signo)
+{
+}
+
+pid_t	getpid(void)
+{
+}
+
+int main(int argc, char **argv)
+{
+}
