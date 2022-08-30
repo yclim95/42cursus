626ee68e79a11d49e9ab3505e84e431495d8c2f5
Lim Yao cheng <lyao-che@u81z05s02.42kl.edu.my>
2022-08-30 10:51:57 +0800

    Added: child.c

---
+++ 05_pipex/mandatory/child.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyao-che <lyao-che@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 10:43:29 by lyao-che          #+#    #+#             */
+/*   Updated: 2022/08/30 10:51:26 by lyao-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static char	*find_cmd(char **paths, char *cmd)
+{
+}
+
+void	execute_1st_child(t_pipex pipex, char *argv[], char *envp[])
+{
+}
+
+void	execute_2nd_child(t_pipex pipex, char *argv[], char *envp[])
+{
+}
